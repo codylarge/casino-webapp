1fcfea7f02315c4b358fa13e4b57ff379429f090
Cody Large <dlarge1@ewu.edu>
2023-12-25 09:51:55 -0800

    wheel updates
    still not working properly, but better

--- src/scripts/wheel.js
+++ src/scripts/wheel.js
@@ -2,41 +2,47 @@ const colorSegments = [
   { color: "red", range: [0, 15] },
   { color: "yellow", range: [15, 30] },
   { color: "green", range: [30, 45] },
-  { color: "blue", range: [45, 60] },
-  { color: "hotpink", range: [60, 75] },
+  { color: "yellow", range: [45, 60] },
+  { color: "blue", range: [60, 75] },
   { color: "yellow", range: [75, 90] },
   { color: "green", range: [90, 105] },
   { color: "yellow", range: [105, 120] },
   { color: "hotpink", range: [120, 135] },
   { color: "yellow", range: [135, 150] },
   { color: "green", range: [150, 165] },
-  { color: "blue", range: [165, 180] },
-  { color: "yellow", range: [180, 195] },
-  { color: "hotpink", range: [195, 210] },
+  { color: "yellow", range: [165, 180] },
+  { color: "blue", range: [180, 195] },
+  { color: "yellow", range: [195, 210] },
   { color: "blue", range: [210, 225] },
   { color: "green", range: [225, 240] },
   { color: "yellow", range: [240, 255] },
   { color: "hotpink", range: [255, 270] },
   { color: "yellow", range: [270, 285] },
   { color: "green", range: [285, 300] },
-  { color: "blue", range: [300, 315] },
-  { color: "yellow", range: [315, 330] },
-  { color: "green", range: [330, 345] },
-  { color: "blue", range: [345, 360] },
+  { color: "yellow", range: [300, 315] },
+  { color: "blue", range: [315, 330] },
+  { color: "yellow", range: [330, 345] },
+  { color: "green", range: [345, 360] },
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
   const wheel = document.getElementById("wheel");
+  const wheelContainer = document.querySelector(".wheel-container");
   const spinButton = document.getElementById("spin-button");
 
   spinButton.addEventListener("click", function () {
-    const randomDegree = Math.floor(Math.random() * 1080) + 1080;
+    const randomSeed = Math.floor(Math.random() * 360);
+    const spins = Math.floor(Math.random() * 10) + 2; // Adjust the number of spins
+    const randomDegree = spins * 360 + randomSeed;
 
-    // Apply the rotation animation to the wheel
-    wheel.style.transform = `rotate(${randomDegree}deg)`;
+    console.log("RandomDegree: " + randomDegree);
+    // Apply the rotation animation to the wheel container
+    wheel.style.transition = "transform 3s ease-out"; // Adjust the transition duration
+    wheel.style.transform = `rotate(${randomDegree}deg) rotateX(0) rotateY(0)`;
 
     // Determine the color based on the final angle
-    const finalAngle = ((randomDegree % 1080) + 1080) % 1080; // Ensure positive angle
+    const finalAngle = ((randomDegree % 360) + 360) % 360; // Ensure positive angle
+    console.log("Final Angle: " + finalAngle);
     const color = getColorByAngle(finalAngle);
 
     // Log or handle the color as needed
@@ -48,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const segment = colorSegments.find(
       (segment) => angle >= segment.range[0] && angle < segment.range[1]
     );
-
+    console.log("segment range: " + segment.range[0] + " " + segment.range[1]);
     // Return the color of the matched segment
     return segment ? segment.color : null;
   }
