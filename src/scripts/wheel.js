f3d6aa4b70f80466fe24e09922ef14ee3ea0a51c
Cody Large <dlarge1@ewu.edu>
2024-02-16 09:10:54 -0800

    .

--- src/scripts/wheel.js
+++ src/scripts/wheel.js
@@ -1,29 +1,29 @@
 // Beautifully hardcoded color segments
 const colorSegments = [
-  { color: "red", range: [0, 15] },
-  { color: "yellow", range: [15, 30] },
-  { color: "green", range: [30, 45] },
-  { color: "yellow", range: [45, 60] },
-  { color: "blue", range: [60, 75] },
-  { color: "yellow", range: [75, 90] },
-  { color: "green", range: [90, 105] },
-  { color: "yellow", range: [105, 120] },
-  { color: "hotpink", range: [120, 135] },
-  { color: "yellow", range: [135, 150] },
-  { color: "green", range: [150, 165] },
-  { color: "yellow", range: [165, 180] },
-  { color: "blue", range: [180, 195] },
-  { color: "yellow", range: [195, 210] },
-  { color: "blue", range: [210, 225] },
-  { color: "green", range: [225, 240] },
-  { color: "yellow", range: [240, 255] },
-  { color: "hotpink", range: [255, 270] },
-  { color: "yellow", range: [270, 285] },
-  { color: "green", range: [285, 300] },
-  { color: "yellow", range: [300, 315] },
-  { color: "blue", range: [315, 330] },
-  { color: "yellow", range: [330, 345] },
-  { color: "green", range: [345, 360] },
+  { color: "red", range: [0, 15], mult: 20 },
+  { color: "yellow", range: [15, 30], mult: 1 },
+  { color: "green", range: [30, 45], mult: 3 },
+  { color: "yellow", range: [45, 60], mult: 1 },
+  { color: "blue", range: [60, 75], mult: 5 },
+  { color: "yellow", range: [75, 90], mult: 1 },
+  { color: "green", range: [90, 105], mult: 3 },
+  { color: "yellow", range: [105, 120], mult: 1 },
+  { color: "hotpink", range: [120, 135], mult: 10 },
+  { color: "yellow", range: [135, 150], mult: 1 },
+  { color: "green", range: [150, 165], mult: 3 },
+  { color: "yellow", range: [165, 180], mult: 1 },
+  { color: "blue", range: [180, 195], mult: 5 },
+  { color: "yellow", range: [195, 210], mult: 1 },
+  { color: "blue", range: [210, 225], mult: 5 },
+  { color: "green", range: [225, 240], mult: 3 },
+  { color: "yellow", range: [240, 255], mult: 1 },
+  { color: "hotpink", range: [255, 270], mult: 10 },
+  { color: "yellow", range: [270, 285], mult: 1 },
+  { color: "green", range: [285, 300], mult: 3 },
+  { color: "yellow", range: [300, 315], mult: 1 },
+  { color: "blue", range: [315, 330], mult: 5 },
+  { color: "yellow", range: [330, 345], mult: 1 },
+  { color: "green", range: [345, 360], mult: 3 },
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -56,13 +56,15 @@ const spinTo = (angle) => {
   // Calculate the final angle after the animation
   const finalAngle = angle % 360;
 
+  // Spin timeout, ensures spin is complete and results are outputted before resetting
   setTimeout(() => {
-    const spinButton = document.getElementById("spin-button");
+    const lastColor = document.getElementById("lastColor");
+
     // Determine the color based on the final angle
     const color = getColorByAngle(Math.abs(finalAngle));
-    // Output the result
-    console.log("Landed on color:", color);
-    spinButton.classList.remove("unclickable");
+
+    lastColor.innerHTML = "Last spin: " + color;
+
     setTimeout(resetWheel, 3000);
   }, 6000); // Adjust to match transition duration
 };
@@ -79,6 +81,8 @@ const getColorByAngle = (angle) => {
 
 const resetWheel = () => {
   const wheel = document.getElementById("wheel");
+  const spinButton = document.getElementById("spin-button");
   wheel.style.transition = "none"; // Disables transition for instant reset
   wheel.style.transform = "rotate(0deg)";
+  spinButton.classList.remove("unclickable");
 };
